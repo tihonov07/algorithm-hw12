e052fdd52a26329e62f3126ac5b477aff90abb8e
aleksandr <Aleksandr.A.Tikhonov@rt.ru>
2024-12-01 13:42:46 +0300

    lesson 12

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -5,6 +5,9 @@ import lombok.SneakyThrows;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HomeWork {
 
@@ -14,8 +17,36 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
+        try (var sc = new Scanner(in); var pw = new PrintWriter(out)) {
+            int N = sc.nextInt();
+
+            for (int i = 0; i < N; i++) {
+                var input = sc.next();
+                pw.println(minimalD(input));
+            }
+        }
     }
 
 
+
+    static int minimalD(String s) {
+        int n = s.length();
+
+        var pos = new ArrayList<Integer>();
+        pos.add(0);
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'R') {
+                pos.add(i + 1);
+            }
+        }
+        pos.add(n + 1);
+
+        int ans = 0;
+        for (int i = 0; i < pos.size() - 1; i++) {
+            ans = Math.max(ans, pos.get(i + 1) - pos.get(i));
+        }
+
+        return ans;
+    }
 
 }
